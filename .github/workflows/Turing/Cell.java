e68ca585e85350658941a2952c059ddeaabb3cc2
Berleo1985 <97443327+Berleo1985@users.noreply.github.com>
2022-01-10 14:49:02 +0200

    Delete .github/workflows directory

--- .github/workflows/Turing/Cell.java
+++
@@ -1,7 +0,0 @@
-package com.Turing;
-
-public class Cell {
-    public char content;  // The character in this cell.
-    public Cell next;// Pointer to the cell to the right of this one.
-    public Cell prev;// Pointer to the cell to the left of this one.
-}
